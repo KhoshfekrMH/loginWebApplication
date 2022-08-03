f9f9cf81771a3097af42499ae650c81aeab5aed6
KhoshfekrMH <khoshfekr2001@protonmail.com>
2022-08-04 01:41:50 +0430

    clean some codes and add place holder for password and password repeat

--- signup.php
+++ signup.php
@@ -16,33 +16,17 @@ require "header.php"
     </div>
 
     <div class="form-floating">
-      <input type="password" name="pwd" class="form-control middle" id="PasswordInput">
+      <input type="password" name="pwd" class="form-control middle" id="PasswordInput" placeholder="Password">
       <label for="PasswordInput">Password</label>
     </div>
 
     <div class="form-floating">
-      <input type="password" name="pwd-repeat" class="form-control bottom" id="PasswordRepeat">
+      <input type="password" name="pwd-repeat" class="form-control bottom" id="PasswordRepeat" placeholder="Password Repeat">
       <label for="PasswordRepeat">Password Repeat</label>
     </div>
 
     <button type="submit" name="signup-submit" class="w-100 btn btn-lg btn-primary">SignUp</button>
   </form>
-<!-- <form class="formCenter" action="includes/signup.inc.php" merthod="POST">
-    <h1 class="h3 mb-3 fw-normal">Please sign in!</h1>
-
-
-    <div class="form-floating">
-      <input type="password" name="pwd" class="form-control middle" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-
-    <div class="form-floating">
-      <input type="password" name="pwd-repeat" class="form-control bottom" id="floatingPassword" placeholder="Repeat Password">
-      <label for="floatingPassword">Repeat Password</label>
-    </div>
-
-    <button class="w-100 btn btn-lg btn-primary" type="submit" name="signup-submit">SignUp</button>
-</form> -->
 </main>
 
 <?php
