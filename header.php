4b14f1eb572f1ae84c2f9ef58c18d7996816250e
KhoshfekrMH <khoshfekr2001@protonmail.com>
2022-08-03 18:08:33 +0430

    Header completed!

--- header.php
+++ header.php
@@ -1,28 +1,53 @@
-<header>
-    <nav>
-        
-        <a href="#">
-            <img src="img/SignUp.png" alt="" class="logo">
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Home Page</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <header class="p-3 text-bg-dark">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/loginWebApplication" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img src="img/SignUp.png" class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap" alt="signUp icon">
         </a>
-        
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">About Me</a></li>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="index.php" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
         </ul>
 
-        <div>
+
+        <div class="text-end inlineHeader">
             <form action="includes/login.inc.php" method="post">
-                <input type="text" name="mailuid" placeholder="E-mail/username...">
-                <input type="password" name="pwd" placeholder="password...">
-                <button type="submit" name="login-submit">Login</button>
+                <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                <input type="password" name="pwd" placeholder="Password">
+                <button type="submit" class="btn btn-outline-light me-2">Login</button>
             </form>
-            <a href="signup.php">SignUp</a>
-            <form action="includes/logout.inc.php">
-                <button type="submit" name="logout-submit">LogOut</button>
+            <button type="button" class="btn btn-warning"><a href="signup.php">Sign-up</a></button>
+
+            <form action="includes/logout.inc.php" method="post">
+                <button type="submit" class="btn btn-outline-light me-2">Logout</button>
             </form>
         </div>
-    </nav>
 
+      </div>
+    </div>
+  </header>
+    
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
 
-</header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+ </body>
+</html>
--- index.php
+++ index.php
@@ -1,26 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up Page</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
 <?php
 require "header.php"
 ?>
 
 <main>
-    <p>You are logged in!</p>
-    <p>You are logged Out!</p>
+    <div class="mx-auto alert alert-info" role="alert" style="margin-top: 10px; width: 200px;">
+        you are logged out!
+    </div>
+
+    <div class="mx-auto alert alert-info" role="alert" style="width: 200px;">
+        you are logged in!
+    </div>
 </main>
+
+
 
 <?php
 require "footer.php"
 ?>
-
-</body>
-</html>
